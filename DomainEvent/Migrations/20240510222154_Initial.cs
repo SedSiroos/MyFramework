e684c15c92cc6448231359e3b7c90ee9dc364c12
s.ghalyani <--list>
2024-05-11 02:00:00 +0330

    outbox

---
+++ DomainEvent/Migrations/20240510222154_Initial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DomainEvent.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutBoxEvent",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccuredUserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AccuredOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AggragateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AggragateTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AggragateId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventPayload = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsProcessed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutBoxEvent", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Family = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutBoxEvent");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ Framework.Domain/OutBoxEventItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Framework.Domain;
+
+public class OutBoxEventItem
+{
+    public long Id { get; set; }
+    public Guid EventId { get; set; }
+    public string AccuredUserId { get; set; }
+    public DateTime AccuredOn { get; set; }
+    public string AggragateName { get; set; }
+    public string AggragateTypeName { get; set; }
+    public string AggragateId { get; set; }
+    public string EventName { get; set; }
+    public string EventTypeName { get; set; }
+    public string EventPayload { get; set; }
+    public bool IsProcessed { get; set; }
+}
