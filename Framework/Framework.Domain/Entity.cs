5d747e4483b9294de4892dd3b893377479538c4b
ghalyani <siroos.ghal@gmail.com>
2024-05-11 14:19:34 +0330

    initial framework.domain

---
+++ Framework/Framework.Domain/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace Framework.Domain;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
+    public IReadOnlyList<IDomainEvent> Events => _events;
+    public void AddEvent(IDomainEvent @event)
+    {
+        _events.Add(@event);
+    }
+    public void ClearEvent()
+    {
+        _events.Clear();
+    }
+}
