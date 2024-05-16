66d0c0554d9a10464b88234db2cd5e2dfb12fc8e
s.ghalyani <--list>
2024-05-16 19:25:30 +0330

    aa

--- Framework.Domain/obj/Debug/net6.0/Framework.Domain.AssemblyInfo.cs
+++ Framework.Domain/obj/Debug/net6.0/Framework.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Framework.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01347e756b0fe3a913e2cdb0c6cb7a2ace95b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4571c47db0854bfed190c54c8280b7c0362870a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Framework.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Framework.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
