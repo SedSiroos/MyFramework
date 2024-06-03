57d58a17cdc655ff21c03ddb6e9ea8f4993a1982
s.ghalyani <--list>
2024-06-03 21:38:04 +0330

    change folder

--- Framework.Domain/obj/Debug/net6.0/Framework.Domain.AssemblyInfo.cs
+++ Framework.Domain/obj/Debug/net6.0/Framework.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Framework.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4571c47db0854bfed190c54c8280b7c0362870a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d0c0554d9a10464b88234db2cd5e2dfb12fc8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Framework.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Framework.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
